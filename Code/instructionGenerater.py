51f495b1d94f4769e6ab4390fa7a9b2f9a62c7f9
Nate Foss <npfoss@gmail.com>
2016-08-09 18:34:31 -0400

    make edge finding greedy and generate ytjmit eaw

--- Code/instructionGenerater.py
+++ Code/instructionGenerater.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 WIDTH = 0
 HEIGHT = 0
 idealTotalVertSteps = 15555
-pixScale = 20 # NOTE: will have to change based on image size
+pixScale = 21 # NOTE: will have to change based on image size
 DIRECTIONS = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
 #--------------------------------------------------Instruction File Generator--
 def input(filename):
@@ -20,14 +20,24 @@ def input(filename):
     txt = open ( filename ) . read() . split()
 
     txt.pop(0)
+    nextthing = txt.pop(0)
+    while not isInt(nextthing):
+        nextthing = txt.pop(0)
 
-    WIDTH = int(txt.pop(0))
+    WIDTH = int(nextthing)
     HEIGHT = int(txt.pop(0))
     maxRGB = txt.pop(0)
 
     img = [[1 if int(txt[(row*WIDTH + col)*3]) >= 250 else 0 for col in range( WIDTH )] for row in range(HEIGHT)]
 
     return img
+#--------------------------------------------------Instruction File Generator--
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 #--------------------------------------------------Instruction File Generator--
 def outputImage(filename, img):
     outfile  = open ( filename , 'w' )
@@ -54,12 +64,10 @@ def followLineAndPrint(img, drawn, row, col, outfile): #erases from original as 
     adj = adjacentPixels(img, row, col)
     while adj:
         dr , dc = 0, 0
-        if len(adj) > 1:
-            # have to choose where to go
-            for d in adj:
-                if not (d[0] and d[1]):
-                    (dr, dc) = d
-                    break
+        for d in adj:
+            if not (d[0] and d[1]):
+                (dr, dc) = d
+                break
         if not (dr or dc):
             (dr, dc) = adj[0]
         outfile.write(str(DIRECTIONS.index((dr,dc))) + ' ' + str(pixScale) + '\n')
@@ -82,12 +90,24 @@ def moveWithPenUp(r1, c1, r2, c2, outfile):
     outfile.write(str(dirx) + ' ' + str(pixScale * max(c1-c2,c2-c1)) + '\n')
     outfile.write(str(diry) + ' ' + str(pixScale * max(r1-r2,r2-r1)) + '\n')
     #put pen down
-    outfile.write('9 1\n')
+    outfile.write('9 1\n') 
+#--------------------------------------------------Instruction File Generator--
+def spiral(X, Y):
+    x = y = 0
+    dx = 0
+    dy = -1
+    for i in range(max(X, Y)**2):
+        if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
+            print (x, y)
+            yield (x,y)
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
 #===============================< MAIN >========================================
 def main():
     filename = sys.argv[1]
     img = input(filename)
-    pixScale = idealTotalVertSteps // HEIGHT
+    #risky pixScale = idealTotalVertSteps // HEIGHT
     outfile = open(filename[:-4] + '.eaw', 'w')
     drawn = [[0 for col in range(WIDTH)] for row in range(HEIGHT)]
 
@@ -95,7 +115,6 @@ def main():
     pr = 0
     pc = 0
 
-    count = 0
     for row in range(HEIGHT):
         for col in range(WIDTH):
             if img[row][col]:
@@ -103,7 +122,20 @@ def main():
                 moveWithPenUp(pr,pc,row,col, outfile)
                 pr, pc = followLineAndPrint(img, drawn, row, col, outfile)
                 #outputImage(filename[:-4] + str(count) + '.ppm', drawn) #for debugging
-                count += 1
+                break;
+        if pr != 0 or pc != 0:
+            break;
+    opr = 0
+    opc = 0
+    while opr != pr and opc != pc:
+        for (dr , dc) in spiral(max(HEIGHT-pr, pr), max(WIDTH -pc, pc)):
+            if 0 <= pr + dr < HEIGHT and 0 <= pc + dc < WIDTH and img[pr+dr][pc+dc]:
+                                print('it happened')
+                                moveWithPenUp(pr,pc,pr+dr,pc +dc, outfile)
+                                pr, pc = followLineAndPrint(img, drawn, pr+dr, pc+dc, outfile)
+                                break
+        opr = pr
+        opc = pc
 
     outfile.close()
 #--------------------------------------------------Instruction File Generator--
